8501dbe94daa016ed8fe7e2f4911b9b34398ff34
Quentin Laval <quentin.laval@etu.univ-smb.fr>
2024-01-30 08:52:17 +0100

    Property

---
+++ Models/Devise.cs
@@ -0,0 +1,20 @@
+﻿namespace WPSConvertisseur.Models
+{
+    public class Devise
+    {
+        public Devise(int id, string nomDevise, double taux)
+        {
+            this.Id = id;
+            this.NomDevise = nomDevise;
+            this.Taux = taux;
+        }
+        public Devise()
+        {
+            
+        }
+
+        public int Id { get; set; }
+        public string NomDevise { get; set; }
+        public double Taux { get; set; }
+    }
+}
